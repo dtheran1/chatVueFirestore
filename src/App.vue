f67d4ff36d9926c23d97b5c987ada9736e2e9b87
Dtheran <dtheran1@gmail.com>
2022-07-05 15:10:28 -0500

    auth

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-app-bar color="primary">
       <v-app-bar-title> Chat Firebase </v-app-bar-title>
       <v-btn
+        v-if="!userGoogle"
         prepend-icon="mdi-google-downasaur"
         color="white"
         plain
@@ -11,11 +12,24 @@
       >
         Acceder
       </v-btn>
+      <v-btn
+        v-if="userGoogle"
+        prepend-icon="mdi-google-downasaur"
+        color="white"
+        plain
+        variant="outlined"
+        @click="logout"
+      >
+        Log out
+      </v-btn>
     </v-app-bar>
 
     <v-main app>
       <v-container>
-        <Chat />
+        <Chat v-if="userGoogle" />
+        <div v-else>
+          <h1 class="text-center mt-15">Debes iniciar sesión para ver los chats</h1>
+        </div>
       </v-container>
     </v-main>
 
@@ -26,19 +40,31 @@
 </template>
 
 <script setup>
-import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "@firebase/auth";
 import { auth } from "./firebase";
 
 import Chat from "./components/Chats.vue";
 import FormAdd from "./components/FormAdd.vue";
+import { ref } from "vue"
+
+const userGoogle = ref(false)
 
 const googleAccess = async () => {
   try {
     const provider = new GoogleAuthProvider();
     const { user } = await signInWithPopup(auth, provider);
-    console.log(user);
+    // console.log(user);
   } catch (error) {
     console.log(error);
   }
 };
+
+const logout = async() => {
+  await signOut(auth)
+}
+
+onAuthStateChanged(auth, user => {
+  console.log(user)
+  userGoogle.value = user
+})
 </script>
