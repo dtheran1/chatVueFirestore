1e3a4f9dc37cdcf246e8b5e52ba10393c98bad7d
Dtheran <dtheran1@gmail.com>
2022-07-05 14:53:17 -0500

    add auth google

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,20 @@
   <v-app>
     <v-app-bar color="primary">
       <v-app-bar-title> Chat Firebase </v-app-bar-title>
-      <v-btn prepend-icon="mdi-google-downasaur" color="white" plain variant="outlined"> Acceder </v-btn>
+      <v-btn
+        prepend-icon="mdi-google-downasaur"
+        color="white"
+        plain
+        variant="outlined"
+        @click="googleAccess"
+      >
+        Acceder
+      </v-btn>
     </v-app-bar>
 
     <v-main app>
       <v-container>
-        <Chat />        
+        <Chat />
       </v-container>
     </v-main>
 
@@ -18,7 +26,19 @@
 </template>
 
 <script setup>
-import Chat from "./components/Chats.vue"
-import FormAdd from "./components/FormAdd.vue"
+import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
+import { auth } from "./firebase";
 
+import Chat from "./components/Chats.vue";
+import FormAdd from "./components/FormAdd.vue";
+
+const googleAccess = async () => {
+  try {
+    const provider = new GoogleAuthProvider();
+    const { user } = await signInWithPopup(auth, provider);
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
